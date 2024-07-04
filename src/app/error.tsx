f5103319d82a553603b8ce22fc21a2c6d2ba7696
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-07-04 15:30:35 +0100

    Recovering from Error - usando o reset para lidar com erros

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,17 +1,19 @@
 "use client"
 
 type Props = {
-  error: Error
+  error: Error;
+  reset: () => void;
 }
 
-export default function Error({error}:Props) {
+export default function Error({error,reset}:Props) {
   return (
-    <main className="flex h-screen bg-gray-600">
+    <main className="flex h-screen bg-gray-300">
       <article className="bg-white flex flex-col gap-2 m-auto p-4 max-w-sm w-full rounded-md">
         <h1 className="text-2xl">{"Error"}</h1>
         <p>
           {error.message}
         </p>
+        <button onClick={reset} className="p-2 rounded bg-black text-white">Reset</button>
       </article>
     </main>
   );
