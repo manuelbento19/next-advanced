d99570a8afcb87535d0b8739d47f387a6286af1a
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-07-04 12:01:59 +0100

    Routing: Link Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <main>
       <h1>
         Page Home
       </h1>
+      <ul>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/docs"}>Docs</Link>
+        </li>
+        <li>
+          <Link href={"/products"}>Products</Link>
+        </li>
+      </ul>
     </main>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Products"
@@ -10,6 +11,20 @@ export default function Page() {
         <h1>
           Page Products
         </h1>
+        <ul>
+          <li>
+            <Link href={"/products/1"}>Product 1</Link>
+          </li>
+          <li>
+            <Link href={"/products/2"}>Product 2</Link>
+          </li>
+          <li>
+            <Link href={"/products/3"} replace>Product 3</Link>
+          </li>
+          <li>
+            <Link href={"/products/100"}>Product 100</Link>
+          </li>
+        </ul>
       </main>
     );
 }
