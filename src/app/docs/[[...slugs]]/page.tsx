7fb5603b2d6440d9adf0aa4cfbb95e619ecd5fe3
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-07-04 10:32:03 +0100

    Catch-all segments - Optional

--- src/app/docs/[[...slugs]]/page.tsx
+++ src/app/docs/[[...slugs]]/page.tsx
@@ -5,5 +5,5 @@ export default function Page({params}:{params: {slugs: string[]}}) {
   if(params.slugs?.length==1)
   return <h1>Catching all segments from Docs page - We are on docs of Windows </h1>  
   
-  return <h1>Catching all segments from Docs page - We are on docs of Mac OS </h1>  
+  return <h1>Catching all segments from Docs page </h1>  
 }
