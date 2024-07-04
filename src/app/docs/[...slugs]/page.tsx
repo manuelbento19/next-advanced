c0ffe1ed6705c84cd1a0867a7408c7778ff74972
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-07-04 10:16:00 +0100

    Catch-all segments - Pegando todos os segmentos da rota

---
+++ src/app/docs/[...slugs]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page({params}:{params: {slugs: string[]}}) {
+  if(params.slugs?.length==2)
+  return <h1>Catching all segments from Docs page - We are on docs of Linux </h1>  
+  
+  if(params.slugs?.length==1)
+  return <h1>Catching all segments from Docs page - We are on docs of Windows </h1>  
+  
+  return <h1>Catching all segments from Docs page - We are on docs of Mac OS </h1>  
+}
