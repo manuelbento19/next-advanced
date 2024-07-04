d3996e38460e4e829933bfb450cca5c121eed269
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-07-04 14:05:15 +0100

    Active Links - usePathname hook

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,39 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
+const links = [
+    {
+        label: "Register",
+        path: "/register" 
+    },
+    {
+        label: "Login",
+        path: "/login" 
+    },
+    {
+        label: "Forgot Password",
+        path: "/forgot-password" 
+    }
+]
+
 export default function Layout({children}:{children: ReactNode}){
+    const pathName = usePathname();
 
     return(
         <div>
             <header style={{backgroundColor: "#272592",color: "#fff", paddingBlock: 20,textAlign: 'center'}}>
-                Header of App
+                <ul className="flex items-center gap-2">
+                    {links.map(item=>{
+                        const active = pathName.startsWith(item.path);
+                        return (
+                            <li key={item.path}>
+                                <Link href={item.path} className={active ? "text-red-500": "text-white"}>{item.label}</Link>
+                            </li>
+                        )
+                    })}
+                </ul>
             </header>
             <section style={{paddingBlock: 20}}>
                 {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,6 @@ export default function Page() {
       <h1>
         Page Home
       </h1>
-      <ul>
-        <li>
-          <Link href={"/about"}>About</Link>
-        </li>
-        <li>
-          <Link href={"/blog"}>Blog</Link>
-        </li>
-        <li>
-          <Link href={"/docs"}>Docs</Link>
-        </li>
-        <li>
-          <Link href={"/products"}>Products</Link>
-        </li>
-      </ul>
     </main>
   );
 }
