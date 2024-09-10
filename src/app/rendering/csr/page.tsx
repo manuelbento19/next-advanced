cb5309c81e52ebf7d061e5f865493ac70ca2958e
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-09-10 10:04:32 +0100

    feat: client components

---
+++ src/app/rendering/csr/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from 'react';
+
+export default function Page() {
+  const [count, setCount] = useState(0)
+ 
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
